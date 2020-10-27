fe813db19ffb9dac69502d717f9dd1958d4c7759
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-10-27 14:28:46 +0900

    Update shell

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 df
+pwd
 ls -l
+
 cd efs
+pwd
 ls -l
-export PIPENV_VENV_IN_PROJECT=1
-pipenv sync
+
+cp ../Pipfile Pipfile
+cp ../Pipfile.lock Pipfile.lock
+
+HASH=`shasum -a 256 ../Pipfile.lock |  awk '{print $1}'`
+echo $HASH
+
+if [ -d ${HASH} ]; then
+  echo "Folder already exists"
+else
+  mkdir $HASH
+  cd $HASH
+  cp ../Pipfile Pipfile
+  cp ../Pipfile.lock Pipfile.lock
+  export PIPENV_VENV_IN_PROJECT=1
+  pipenv sync
+fi
